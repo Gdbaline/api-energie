8b2c2a034fe68ce0774f561fd6f7be62a83cae1a
Gdbaline <a.groutdebeaufort@gmail.com>
2025-07-23 00:29:05 +0200

    Create ecowatt.js

---
+++ api/api/ecowatt.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/nouveau_signal_ecowatt/records?limit=20");
+  const data = await response.json();
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json(data);
+}
