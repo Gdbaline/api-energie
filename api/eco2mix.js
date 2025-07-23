2db8cadf3bad0a61d95110284b2d5ec7a81a5da6
Gdbaline <a.groutdebeaufort@gmail.com>
2025-07-23 13:49:58 +0200

    Update eco2mix.js

--- api/eco2mix.js
+++ api/eco2mix.js
@@ -1,7 +1,12 @@
-export default async function handler(req, res) {
-  const response = await fetch("https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/eco2mix-metropoles-tr/records?limit=20");
-  const data = await response.json();
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(data);
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    const response = await fetch('https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/eco2mix-national-tr/exports/json?limit=-1&timezone=UTC&use_labels=false&compressed=false&epsg=4326');
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Erreur API Eco2Mix:', error.message);
+    res.status(500).json({ error: 'Erreur lors de la récupération des données Eco2Mix' });
+  }
 }
